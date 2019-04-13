f9112fe11df0712a637c3c62af018bb8be2a4ba6
杨云召 <yangyunzhao@bcbook.cn>
2019-04-13 09:42:13 +0800

    a commit

--- app/src/main/java/com/example/testa/MainActivity.java
+++ app/src/main/java/com/example/testa/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Utils.dp2px(20f);
+        float a = Utils.dp2px(20f);
     }
 }
