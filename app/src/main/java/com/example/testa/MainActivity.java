8a8d97a9801e7162f7aa5afc0d4acf4aedc2a055
杨云召 <yangyunzhao@bcbook.cn>
2019-04-13 09:34:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/testa/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.testa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.testbase.Utils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Utils.dp2px(20f);
+    }
+}
